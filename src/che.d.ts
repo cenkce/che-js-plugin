d4725e1b6f318668ad7ef26653851e82ad12fda6
Yevhen Vydolob <evidolob@codenvy.com>
2017-12-06 15:42:19 +0200

    add Path class, add content and update content methods to VirtualFile interface

--- src/che.d.ts
+++ src/che.d.ts
@@ -144,8 +144,8 @@ declare interface Project {
      * @param type the project type to check
      * @return true if given project type is applicable to current project
      */
-    isTypeOf(type : String):boolean;
-    
+    isTypeOf(type: String): boolean;
+
     /**
      * Returns the attribute value for given {@code key}. If such attribute doesn't exist, {@code
      * null} is returned. If there is more than one value exists for given {@code key}, than first
@@ -154,7 +154,7 @@ declare interface Project {
      * @param key the attribute name
      * @return first value for the given {@code key} or null if such attribute doesn't exist
      */
-    getAttribute(key:String) : String;
+    getAttribute(key: String): String;
 
     /**
      * Returns the list of attributes for given {@code key}. If such attribute doesn't exist, {@code
@@ -163,7 +163,7 @@ declare interface Project {
      * @param key the attribute name
      * @return the list with values for the given {@code key} or null if such attribute doesn't exist
      */
-    getAttributes(key:String) : String[];
+    getAttributes(key: String): String[];
 
 
 }
@@ -174,8 +174,8 @@ declare interface CurrentUser {
 }
 
 declare interface EditorManager {
-    addFileOperationListener(listener: { (event: FileOperationEvent): void }):Disposable;
-    addEditorOpenedListener(listener: { (event: EditorOpenedEvent): void }):Disposable;
+    addFileOperationListener(listener: { (event: FileOperationEvent): void }): Disposable;
+    addEditorOpenedListener(listener: { (event: EditorOpenedEvent): void }): Disposable;
 }
 
 declare interface EditorOpenedEvent {
@@ -184,7 +184,7 @@ declare interface EditorOpenedEvent {
 }
 
 declare interface EditorPartPresenter {
- //TODO 
+    //TODO 
 }
 
 declare interface FileOperationEvent {
@@ -194,11 +194,13 @@ declare interface FileOperationEvent {
 }
 
 declare interface VirtualFile {
-    getLocation(): String;
+    getLocation(): che.ide.resource.Path;
     getName(): String;
     getDisplayName(): String;
     isReadOnly(): boolean;
     getContentUrl(): String;
+    getContent(): Promise<String>;
+    updateContent(content: String): Promise<void>;
 }
 
 declare interface WorkspaceRuntime {
@@ -336,6 +338,323 @@ declare interface Part {
 
 declare namespace che {
     namespace ide {
+        namespace resource {
+            /**
+             * A path is an ordered collection of string segments, separated by a standard separator
+             * character, "/". A path may also have a leading and/or a trailing separator.
+             *
+             * Note that paths are value objects; all operations on paths return a new path; the path that is
+             * operated on is unscathed.
+             *
+             * This class is not intended to be extended by clients.
+             */
+            class Path {
+                SEPARATOR: string;
+                /**
+                 * Constructs a new path from the given string path. The string path must represent a valid file
+                 * system path on the local file system. The path is canonicalized and double slashes are removed
+                 * except at the beginning. (to handle UNC paths). All forward slashes ('/') are treated as
+                 * segment delimiters, and any segment and device delimiters for the local file system are also
+                 * respected.
+                 */
+                static valueOf(pathString: String): Path;
+
+                /** 
+                 * Returns a new path which is the same as this path but with the given file extension added. If
+                 * this path is empty, root or has a trailing separator, this path is returned. If this path
+                 * already has an extension, the existing extension is left and the given extension simply
+                 * appended. Clients wishing to replace the current extension should first remove the extension
+                 * and then add the desired one.
+                 *
+                 * <p>The file extension portion is defined as the string following the last period (".")
+                 * character in the last segment. The given extension should not include a leading ".".
+                 * @param extension the file extension to append
+                 */
+                addFileExtension(extension: string): Path;
+                /**
+                 * Returns a path with the same segments as this path but with a trailing separator added. This
+                 * path must have at least one segment.
+                 *
+                 * If this path already has a trailing separator, this path is returned.
+                 */
+                addTrailingSeparator(): Path;
+
+                /**
+                 * Returns a path with the same segments as this path but with a leading separator added.
+                 *
+                 * <p>If this path already has a leading separator, this path is returned.
+                 *
+                 * @return the new path
+                 */
+                addLeadingSeparator(): Path;
+
+                /** 
+                 * Returns the canonicalized path obtained from the concatenation of the given path's segments to
+                 * the end of this path. If the given path has a trailing separator, the result will have a
+                 * trailing separator. The device id of this path is preserved (the one of the given path is
+                 * ignored). Duplicate slashes are removed from the path except at the beginning where the path is
+                 * considered to be UNC.
+                 */
+                appendPath(path: Path): Path;
+
+                /** 
+                 * Returns the canonicalized path obtained from the concatenation of the given path's segments to
+                 * the end of this path. If the given path has a trailing separator, the result will have a
+                 * trailing separator. The device id of this path is preserved (the one of the given path is
+                 * ignored). Duplicate slashes are removed from the path except at the beginning where the path is
+                 * considered to be UNC.
+                 */
+                append(path: String): Path;
+
+                equals(obj: any): boolean;
+
+                /**
+                 * Returns the device id for this path, or <code>null</code> if this path has no device id. Note
+                 * that the result will end in ':'.
+                 *
+                 * @return the device id, or <code>null</code>
+                 */
+                getDevice(): String;
+                /**
+                 * Returns the file extension portion of this path, or <code>null</code> if there is none.
+                 *
+                 * <p>The file extension portion is defined as the string following the last period (".")
+                 * character in the last segment. If there is no period in the last segment, the path has no file
+                 * extension portion. If the last segment ends in a period, the file extension portion is the
+                 * empty string.
+                 *
+                 * @return the file extension or <code>null</code>
+                 */
+                getFileExtension(): String;
+                /**
+                 * Returns whether this path has a trailing separator.
+                 *
+                 * <p>Note: In the root path ("/"), the separator is considered to be leading rather than
+                 * trailing.
+                 *
+                 * @return <code>true</code> if this path has a trailing separator, and <code>false</code>
+                 *     otherwise
+                 */
+                hasTrailingSeparator():boolean;
+
+                /**
+                 * Returns whether this path has a leading separator.
+                 *
+                 * <p>Note: In the root path ("/"), the separator is considered to be leading rather than
+                 * trailing.
+                 *
+                 * @return <code>true</code> if this path has a leading separator, and <code>false</code>
+                 *     otherwise
+                 */
+                hasLeadingSeparator():boolean;
+                /**
+                 * Returns whether this path is an absolute path (ignoring any device id).
+                 *
+                 * <p>Absolute paths start with a path separator. A root path, like <code>/</code> or <code>C:/
+                 * </code>, is considered absolute. UNC paths are always absolute.
+                 *
+                 * @return <code>true</code> if this path is an absolute path, and <code>false</code> otherwise
+                 */
+                isAbsolute():boolean;
+
+                /**
+                 * Returns whether this path has no segments and is not a root path.
+                 *
+                 * @return <code>true</code> if this path is empty, and <code>false</code> otherwise
+                 */
+                isEmpty():boolean;
+                /**
+                 * Returns whether this path is a prefix of the given path. To be a prefix, this path's segments
+                 * must appear in the argument path in the same order, and their device ids must match.
+                 *
+                 * <p>An empty path is a prefix of all paths with the same device; a root path is a prefix of all
+                 * absolute paths with the same device.
+                 *
+                 * @param anotherPath the other path
+                 * @return <code>true</code> if this path is a prefix of the given path, and <code>false</code>
+                 *     otherwise
+                 */
+                isPrefixOf(anotherPath: Path): boolean;
+
+                /**
+                 * Returns whether this path is a root path.
+                 *
+                 * <p>The root path is the absolute non-UNC path with zero segments; e.g., <code>/</code> or
+                 * <code>C:/</code>. The separator is considered a leading separator, not a trailing one.
+                 *
+                 * @return <code>true</code> if this path is a root path, and <code>false</code> otherwise
+                 */
+                isRoot(): boolean;
+
+                /**
+                 * Returns a boolean value indicating whether or not this path is considered to be in UNC form.
+                 * Return false if this path has a device set or if the first 2 characters of the path string are
+                 * not <code>Path.SEPARATOR</code>.
+                 *
+                 * @return boolean indicating if this path is UNC
+                 */
+                isUNC(): boolean;
+
+                /**
+                 * Returns the last segment of this path, or <code>null</code> if it does not have any segments.
+                 *
+                 * @return the last segment of this path, or <code>null</code>
+                 */
+                lastSegment() : String;
+
+                /**
+                 * Returns an absolute path with the segments and device id of this path. Absolute paths start
+                 * with a path separator. If this path is absolute, it is simply returned.
+                 *
+                 * @return the new path
+                 */
+                makeAbsolute() : Path;
+
+                /**
+                 * Returns a relative path with the segments and device id of this path. Absolute paths start with
+                 * a path separator and relative paths do not. If this path is relative, it is simply returned.
+                 *
+                 * @return the new path
+                 */
+                makeRelative(): Path;
+
+                /**
+                 * Returns a path equivalent to this path, but relative to the given base path if possible.
+                 *
+                 * <p>The path is only made relative if the base path if both paths have the same device and have
+                 * a non-zero length common prefix. If the paths have different devices, or no common prefix, then
+                 * this path is simply returned. If the path is successfully made relative, then appending the
+                 * returned path to the base will always produce a path equal to this path.
+                 *
+                 * @param base The base path to make this path relative to
+                 * @return A path relative to the base path, or this path if it could not be made relative to the
+                 *     given base
+                 */
+                makeRelativeTo(base: Path): Path;
+                /**
+                 * Returns a count of the number of segments which match in this path and the given path (device
+                 * ids are ignored), comparing in increasing segment number order.
+                 *
+                 * @param anotherPath the other path
+                 * @return the number of matching segments
+                 */
+                matchingFirstSegments(anotherPath : Path): number;
+
+                /**
+                 * Returns a new path which is the same as this path but with the file extension removed. If this
+                 * path does not have an extension, this path is returned.
+                 *
+                 * <p>The file extension portion is defined as the string following the last period (".")
+                 * character in the last segment. If there is no period in the last segment, the path has no file
+                 * extension portion. If the last segment ends in a period, the file extension portion is the
+                 * empty string.
+                 *
+                 * @return the new path
+                 */
+                removeFileExtension():Path;
+
+                /**
+                 * Returns a copy of this path with the given number of segments removed from the beginning. The
+                 * device id is preserved. The number must be greater or equal zero. If the count is zero, this
+                 * path is returned. The resulting path will always be a relative path with respect to this path.
+                 * If the number equals or exceeds the number of segments in this path, an empty relative path is
+                 * returned.
+                 *
+                 * @param count the number of segments to remove
+                 */
+                removeFirstSegments(count: number): Path;
+                /**
+                 * Returns a copy of this path with the given number of segments removed from the end. The device
+                 * id is preserved. The number must be greater or equal zero. If the count is zero, this path is
+                 * returned.
+                 *
+                 * <p>If this path has a trailing separator, it will still have a trailing separator after the
+                 * last segments are removed (assuming there are some segments left). If there is no trailing
+                 * separator, the result will not have a trailing separator. If the number equals or exceeds the
+                 * number of segments in this path, a path with no segments is returned.
+                 *
+                 * @param count the number of segments to remove
+                 */
+                removeLastSegments(count: number) : Path;
+                /**
+                 * Returns a path with the same segments as this path but with a trailing separator removed. Does
+                 * nothing if this path does not have at least one segment. The device id is preserved.
+                 *
+                 * <p>If this path does not have a trailing separator, this path is returned.
+                 *
+                 * @return the new path
+                 */
+                removeTrailingSeparator() : Path;
+
+                /**
+                 * Returns the specified segment of this path, or <code>null</code> if the path does not have such
+                 * a segment.
+                 *
+                 * @param index the 0-based segment index
+                 * @return the specified segment, or <code>null</code>
+                 */
+                segment(index: number): String;
+                /**
+                 * Returns the number of segments in this path.
+                 *
+                 * <p>Note that both root and empty paths have 0 segments.
+                 *
+                 * @return the number of segments
+                 */   
+                segmentCount(): number;
+
+                /**
+                 * Returns the segments in this path in order.
+                 *
+                 * @return an array of string segments
+                 */
+                segments(): String[];
+
+                /**
+                 * Returns a new path which is the same as this path but with the given device id. The device id
+                 * must end with a ":". A device independent path is obtained by passing <code>null</code>.
+                 *
+                 * <p>For example, "C:" and "Server/Volume:" are typical device ids.
+                 *
+                 * @param device the device id or <code>null</code>
+                 * @return a new path
+                 */
+                setDevice(device: String) : Path;
+
+                /**
+                 * Returns a string representation of this path, including its device id. The same separator, "/",
+                 * is used on all platforms.
+                 *
+                 * <p>Example result strings (without and with device id):
+                 *
+                 * <pre>
+                 * "/foo/bar.txt"
+                 * "bar.txt"
+                 * "/foo/"
+                 * "foo/"
+                 * ""
+                 * "/"
+                 * "C:/foo/bar.txt"
+                 * "C:bar.txt"
+                 * "C:/foo/"
+                 * "C:foo/"
+                 * "C:"
+                 * "C:/"
+                 * </pre>
+                 *
+                 * This string is suitable for passing to <code>Path(String)</code>.
+                 *
+                 * @return a string representation of this path
+                 */
+                toString(): String;
+                /**
+                 * Returns a copy of this path with removed last segment.
+                 *
+                 * @return the new path
+                 */
+                parent():Path;
+            }
+        }
         namespace editor {
             enum FileOperation {
                 OPEN,
--- src/events.ts
+++ src/events.ts
@@ -6,10 +6,12 @@ export function handleEvents(ctx: PluginContext) {
 
     ctx.getApi().editorManager.addFileOperationListener(e =>{
         console.log(e.getFile().getLocation() + "@" + e.getOperationType());
-        console.log("Project " + ctx.getApi().appContext.getRootProject());
     })
 
     ctx.getApi().editorManager.addEditorOpenedListener(e =>{
         console.log(e.getFile().getLocation() + "#" + e.getEditor());
+        e.getFile().getContent().then(c =>{
+            console.log(c);
+        })
     });
 }
